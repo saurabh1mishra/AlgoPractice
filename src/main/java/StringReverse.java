5b15a288e0efbd4f7301b4d35dbaf7cb2f710878
Saurabh Mishra <saurabh.mishra@olx.com>
2022-03-31 17:17:12 +0530

    adding docker compose for selenium 4

---
+++ src/main/java/StringReverse.java
@@ -0,0 +1,54 @@
+/*
+
+String actual = “$r$ev$$w”;
+			 0123456
+      //reverse = “w$ve$$r”
+			 0123456
+ */
+public class StringReverse {
+
+    static String rev(String input) {
+        String[] str = input.split("");
+
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (StringReverse.isSString(str[left])) {
+                if (StringReverse.isSString(str[right])) {
+                    swap(str, left, right);
+                }
+                right = right - 1;
+            }
+            left = left + 1;
+        }
+
+        return printArray(str);
+    }
+
+    static void swap(String[] str, int left, int right) {
+        String temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+
+    }
+
+    static boolean isSString(String str) {
+        return str.matches("\\w");
+    }
+
+    static String printArray(String[] str) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : str) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String actual = "r$ev$$w";
+        String rev = rev(actual);
+        System.out.println(rev);
+    }
+}
